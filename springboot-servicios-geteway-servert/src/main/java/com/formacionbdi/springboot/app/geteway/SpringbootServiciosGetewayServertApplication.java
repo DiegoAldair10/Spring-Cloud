7896a5ab4bcd0e8c72f9c519cdb1097001b3d88d
diego <diegotello211098@gmail.com>
2022-07-08 10:52:09 -0500

    Subiendo Proyecto

---
+++ springboot-servicios-geteway-servert/src/main/java/com/formacionbdi/springboot/app/geteway/SpringbootServiciosGetewayServertApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiciosGetewayServertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiciosGetewayServertApplication.class, args);
+	}
+
+}
---
+++ springboot-servicios-geteway-servert/src/test/java/com/formacionbdi/springboot/app/geteway/SpringbootServiciosGetewayServertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiciosGetewayServertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
